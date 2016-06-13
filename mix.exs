28139fb1fa17af7f73781f6cff2d4796455796d2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-14 05:33:00 +0800

    Add expug

--- mix.exs
+++ mix.exs
@@ -7,26 +7,26 @@ defmodule PhoenixExpug.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:expug, git: "git://github.com/rstacruz/expug.git"}
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Rico Sta. Cruz"],
+      licenses: ["MIT"],
+      files: ["lib", "mix.exs", "README.md"],
+      links: %{github: "https://github.com/rstacruz/phoenix_expug"}
+    ]
   end
 end
