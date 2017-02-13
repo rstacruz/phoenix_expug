ca9d3236f056f34f7a3605bc25fe69f862ce7954
Adrian Peterson Co <adrianpco@gmail.com>
2017-02-13 12:40:04 +0800

    Updates syntax for Elixir 1.4.0

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule PhoenixExpug.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     package: package,
-     deps: deps]
+     package: package(),
+     deps: deps()
   end
 
   def application do
