ed153f4cdd0899aafd19011d6859427a9e12b991
Adrian Peterson Co <adrianpco@gmail.com>
2017-02-13 12:43:01 +0800

    Whoops!

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PhoenixExpug.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      package: package(),
-     deps: deps()
+     deps: deps()]
   end
 
   def application do
